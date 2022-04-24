21ff33ae7be6c5148ce0b1daafcd384a76010dac
Montekar <mantas.pasakarnis@gmail.com>
2022-04-24 21:49:32 +0200

    home page v2

--- gym-website-frontend/src/app/app-routing.module.ts
+++ gym-website-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {ContactComponent} from "./contact/contact.component";
+import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
 
   { path: '**', redirectTo: 'home' },
 ];
