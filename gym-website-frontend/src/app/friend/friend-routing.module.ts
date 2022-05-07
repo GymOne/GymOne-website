254bb230001611ee01ab3516f896599b3efa5d03
Danylo <danyloboshagora@gmail.com>
2022-05-07 12:10:25 +0200

    Get friend requests works.

--- gym-website-frontend/src/app/friend/friend-routing.module.ts
+++ gym-website-frontend/src/app/friend/friend-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LogRegComponent} from "../auth/log-reg/log-reg.component";
-import {FriendComponent} from "./friend/friend.component";
+//import {FriendComponent} from "./friend/friend.component";
 
-const routes: Routes = [{path: '', component: FriendComponent}];
+const routes: Routes = [
+  //  {path: '', component: FriendComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
