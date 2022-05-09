53cc18c6987d78d3d7f9cce7315fe658380974f7
Montekar <mantas.pasakarnis@gmail.com>
2022-05-09 13:41:57 +0200

    empty profile

--- gym-website-frontend/src/app/app-routing.module.ts
+++ gym-website-frontend/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import {ContactComponent} from "./contact/contact.component";
 import {AboutComponent} from "./about/about.component";
 import {AuthGuard} from "./auth/guard/auth.guard";
 import {TrackingComponent} from "./tracking/tracking.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]  },
   { path: 'tracking', component: TrackingComponent, canActivate: [AuthGuard]  },
   { path: "auth", loadChildren: () => import('./auth/auth.module').then(f=>f.AuthModule)},
   { path: "friends", loadChildren: () => import('./friend/friend.module').then(f=>f.FriendModule)},
---
+++ gym-website-frontend/src/app/profile/profile.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbModal, NgbModalConfig} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
