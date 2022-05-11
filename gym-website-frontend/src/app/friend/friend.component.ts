8a45f72d53e1842c4f5376d220a0570afc15af92
Truncatedshark <56824915+Truncatedshark@users.noreply.github.com>
2022-05-11 16:03:59 +0200

    Friend now show up when another user has added them

--- gym-website-frontend/src/app/friend/friend.component.ts
+++ gym-website-frontend/src/app/friend/friend.component.ts
@@ -12,7 +12,7 @@ import {FriendDto} from "../shared/dtos/friend.dto";
 })
 export class FriendComponent implements OnInit {
   @Select(AuthState.getEmail) currentEmail : Observable<string>;
-  private email = "";
+  public  email = "";
   public searchEmail;
   public $listFriends = [];
   public $listRequests = [];
@@ -44,7 +44,7 @@ export class FriendComponent implements OnInit {
 
           const friends = value[i];
          this.getUsersEmail(friends.senderId)
-          if (friends.isAccepted == true) {
+          if (friends.isAccepted == true ) {
             this.$listFriends.push(friends);
           } else if(friends.isAccepted == false && friends.senderId != this.email) {
             this.$listRequests.push(friends);
